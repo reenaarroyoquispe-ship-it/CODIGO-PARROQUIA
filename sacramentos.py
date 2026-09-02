8232b34564b7c203d8617b78a1dc9c1f3f5c885f
reena <reenaarroyoquispe@gmail.com>
2026-09-01 20:56:54 -0500

    Add Sacramento and related classes for activities

---
+++ sacramentos.py
@@ -0,0 +1,33 @@
+class Sacramento:
+
+    def __init__(self, beneficiario, fecha, costo):
+        self.beneficiario = beneficiario
+        self.fecha = fecha
+        self.costo = costo
+
+    def mostrar(self):
+        print("Actividad:", self.tipo())
+        print("Beneficiario:", self.beneficiario.nombre)
+        print("Fecha:", self.fecha)
+        print("Costo:", self.costo)
+
+    def tipo(self):
+        return "Sacramento"
+
+
+class Bautizo(Sacramento):
+
+    def tipo(self):
+        return "Bautizo"
+
+
+class Matrimonio(Sacramento):
+
+    def tipo(self):
+        return "Matrimonio"
+
+
+class Retiro(Sacramento):
+
+    def tipo(self):
+        return "Retiro"
