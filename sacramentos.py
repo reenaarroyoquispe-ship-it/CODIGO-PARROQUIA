7884623c1e5f3c84ad24d4f76a461f48c3ab5b2b
reena <reenaarroyoquispe@gmail.com>
2026-09-01 22:14:03 -0500

    Refactor Sacramento class and its subclasses

--- sacramentos.py
+++ sacramentos.py
@@ -1,33 +1,31 @@
 class Sacramento:
 
-    def __init__(self, beneficiario, fecha, costo):
-        self.beneficiario = beneficiario
-        self.fecha = fecha
+    def __init__(self, nombre_sacramento, costo):
+        self.nombre_sacramento = nombre_sacramento
         self.costo = costo
 
-    def mostrar(self):
-        print("Actividad:", self.tipo())
-        print("Beneficiario:", self.beneficiario.nombre)
-        print("Fecha:", self.fecha)
-        print("Costo:", self.costo)
-
-    def tipo(self):
-        return "Sacramento"
+    def mostrar_detalle(self):
+        print("Sacramento:", self.nombre_sacramento)
+        print("Costo de tramitación: S/.", self.costo)
 
 
 class Bautizo(Sacramento):
 
-    def tipo(self):
-        return "Bautizo"
+    def __init__(self, costo, fecha_bautizo):
+        super().__init__("Bautizo", costo)
+        self.fecha_bautizo = fecha_bautizo
+
+    def mostrar_detalle(self):
+        super().mostrar_detalle()
+        print("Fecha programada para el bautizo:", self.fecha_bautizo)
 
 
 class Matrimonio(Sacramento):
 
-    def tipo(self):
-        return "Matrimonio"
+    def __init__(self, costo, iglesia_boda):
+        super().__init__("Matrimonio", costo)
+        self.iglesia_boda = iglesia_boda
 
-
-class Retiro(Sacramento):
-
-    def tipo(self):
-        return "Retiro"
+    def mostrar_detalle(self):
+        super().mostrar_detalle()
+        print("Lugar de la boda:", self.iglesia_boda)
